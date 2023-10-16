17a477882cf147cf8f3201c737cd9ef2b32c2f52
bwbonanno <bradfordbonanno@gmail.com>
2023-10-15 18:41:54 -0700

    Add tests to config

--- src/config.rs
+++ src/config.rs
@@ -38,3 +38,27 @@ impl Config {
             )))
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::Config;
+
+    #[test]
+    fn gets_token_from_secrets() {
+        let data: serde_yaml::Value = serde_yaml::from_str(&r#"{ "Discord": { "Token": "Test123" }}"#).unwrap();
+        let secrets: serde_yaml::Value = serde_yaml::from_str(&r#"{ "Discord": { "Token": "ExpectedValue" }}"#).unwrap();
+
+        let config = Config {data, secrets};
+        assert_eq!(config.get_discord_token().unwrap(), "ExpectedValue");
+    }
+
+    #[test]
+    fn gets_channel_from_data() {
+        let data: serde_yaml::Value = serde_yaml::from_str(&r#"{ "Discord": { "ChannelID": 1 }}"#).unwrap();
+        let secrets: serde_yaml::Value = serde_yaml::from_str(&r#"{ "Discord": { "ChannelID": 2 }}"#).unwrap();
+        
+        let config = Config {data, secrets};
+        assert_eq!(config.get_channel_id().unwrap(), 1);
+    }
+}
